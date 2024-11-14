60111439f7e121a79130a9478fec15378e5fad51
Megestus <75190962+Megestus@users.noreply.github.com>
2024-11-14 10:27:56 +0800

    Update install.py

--- install.py
+++ install.py
@@ -13,8 +13,8 @@ import locale
 
 # Define constants for the toolbox
 TOOLBOX_NAME = "HUGTools"
-TOOLBOX_VERSION = "1.2.2 Beta"  # Update this to match HUGTOOL_VERSION in HUGTools_main.py
-TOOLBOX_ICON = "HUGlogo2.png"
+TOOLBOX_VERSION = "1.2.4 Beta"  # Update this to match HUGTOOL_VERSION in HUGTools_main.py
+TOOLBOX_ICON = "HUGlogo.png"
 TOOLBOX_MAIN_MODULE = "HUGTools_main"
 TOOLBOX_HELP_URL = "https://megestus.github.io/HUGTools/"  
 
